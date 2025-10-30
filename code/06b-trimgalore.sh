7182079ebf57b528b52504447c9e120f2d15636f
Dr. Yaamini R. Venkataraman <yaamini.venkataraman@gmail.com>
2025-10-30 14:45:47 -0700

    try looping files for trimgalore

--- code/06b-trimgalore.sh
+++ code/06b-trimgalore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=compute          								 				  								  	     		# Queue selection
-#SBATCH --job-name=yrv_fastqc         							 															        # Job name
+#SBATCH --job-name=yrv_trimgalore         							 															        # Job name
 #SBATCH --mail-type=ALL              							   				     									     		# Mail events (BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=yaamini.venkataraman@whoi.edu    				    									    		# Where to send mail
 #SBATCH --nodes=1                                                									        # One node
@@ -9,7 +9,9 @@
 #SBATCH --mem=100gb                                                 								      # Job memory request
 #SBATCH --time=24:00:00            								   															     		# Time limit hrs:min:sec
 #SBATCH --output=yrv_trimgalore%j.log  								   															     		# Standard output/error
-#SBATCH --chdir=vortexfs1/scratch/yaamini.venkataraman/wc-green-crab/output/06b-trimgalore/	# Working directory for this script
+#SBATCH --chdir=/vortexfs1/scratch/yaamini.venkataraman/wc-green-crab/output/06b-trimgalore	# Working directory for this script
+
+#Adapted from script by Sam Bogan: https://github.com/snbogan/CP_RNAseq/blob/main/Scripts/CP_cutadapt.txt
 
 #Exit script if any command fails
 set -e
@@ -21,8 +23,8 @@ MULTIQC=/vortexfs1/home/yaamini.venkataraman/miniconda3/bin/multiqc
 python=/vortexfs1/home/yaamini.venkataraman/miniconda3/bin/python
 
 #Directory paths
-DATA_DIR=/vortexfs1/omics/tepolt/raw_reads/C_maenas/Cm_yrv_2023/
-OUTPUT_DIR=/vortexfs1/scratch/yaamini.venkataraman/wc-green-crab/output/06b-trimgalore/
+DATA_DIR=/vortexfs1/omics/tepolt/raw_reads/C_maenas/Cm_yrv_2023
+OUTPUT_DIR=/vortexfs1/scratch/yaamini.venkataraman/wc-green-crab/output/06b-trimgalore
 
 #Trimming
 
@@ -30,16 +32,33 @@ echo "Start TrimGalore"
 
 echo "Trim 1: Remove low-quality reads"
 
-#Remove sequences with quality < 20
-${TRIMGALORE} \
---output_dir ${OUTPUT_DIR} \
---paired \
---fastqc_args \
-"--outdir ${OUTPUT_DIR} \
---threads 28" \
---quality 20 \
---path_to_cutadapt ${CUTADAPT} \
-${DATA_DIR}/*fastq.gz
+#Loop through input files in the directory
+for input_file in ${DATA_DIR}/*_R1_001.fastq.gz; do
+ # Extract the base filename
+  filename=$(basename "$input_file")
+ # Construct the corresponding R2 filename
+  input_r2="${input_file/_R1/_R2}"
+  # Construct the output filenames
+  output_r1="${OUTPUT_DIR}/${filename/R1_001.fastq.gz/_trimmed_R1.fastq}"
+  output_r2="${OUTPUT_DIR}/${filename/R1_001.fastq.gz/_trimmed_R2.fastq}"
+  # Run TrimGalore to trim adapters, remove low-quality sequences, and retain sequences of a specific length
+    ${TRIMGALORE} \
+    --output_dir ${OUTPUT_DIR} \
+    -a "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA" \
+    -A "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT" \
+    --quality 20 \
+    --length 20 \
+    -o $output_r1 \
+    -p $output_r2 \
+    $input_file \
+    $input_r2
+    --fastqc_args \
+    "--outdir ${OUTPUT_DIR} \
+    --threads 28" \
+    --path_to_cutadapt ${CUTADAPT}
+  # Print completion message
+  echo "Trimming complete for $filename"
+done
 
 #Move trimmed files to a new directory
 mkdir ${OUTPUT_DIR}/trim1
@@ -53,7 +72,7 @@ echo "Trim 1 MutiQC"
 ${MULTIQC} \
 ${OUTPUT_DIR}/trim1/*
 
-echo "Trim 2: Remove adapter sequences"
+echo "Trim 2: Remove poly A tails"
 
 #Remove adapter sequences. Specify universal adapters instead of using --illumina
 ${TRIMGALORE} \
@@ -63,8 +82,7 @@ ${TRIMGALORE} \
 "--outdir ${OUTPUT_DIR} \
 --threads 28" \
 --paired \
---adapter AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
---adapter2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+-a "AAAAAAAAAA" \
 --path_to_cutadapt ${CUTADAPT} \
 ${OUTPUT_DIR}/trim1/*fq.gz
 
@@ -81,32 +99,5 @@ ${MULTIQC} \
 ${OUTPUT_DIR}/trim2/*
 
 echo "Trim 2 complete"
-
-echo "Trim 3: Remove Poly A tails"
-
-#Remove poly A tails
-${TRIMGALORE} \
---output_dir ${OUTPUT_DIR} \
---paired \
---fastqc_args \
-"--outdir ${OUTPUT_DIR} \
---threads 28" \
--a "AAAAAAAAAA" \
---path_to_cutadapt ${CUTADAPT} \
-${OUTPUT_DIR}/trim2/*fq.gz
-
-#Move trimmed files to a new directory
-mkdir ${OUTPUT_DIR}/trim3
-mv ${OUTPUT_DIR}/*trimmed_trimmed_trimmed.fq.gz ${OUTPUT_DIR}/trim3/.
-mv ${OUTPUT_DIR}/*trimmed_trimmed_trimmed_fastqc* ${OUTPUT_DIR}/trim3/.
-mv ${OUTPUT_DIR}*_trimmed_trimmed.fq.gz_trimming_report.txt ${OUTPUT_DIR}/trim3/.
-
-echo "Trim 3 MutiQC"
-
-#MultiQC
-${MULTIQC} \
-${OUTPUT_DIR}/trim3/*
-
-echo "Trim 3 complete"
 
 echo "TrimGalore complete"
