4056fc4a9b9c8eaa67e8e4e7cc7546af6bc286a8
Dr. Yaamini R. Venkataraman <yaamini.venkataraman@gmail.com>
2025-10-31 15:12:08 -0700

    multiplex trimming

--- code/06b-trimgalore.sh
+++ code/06b-trimgalore.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --partition=compute          								 				  								  	     		# Queue selection
+#SBATCH --partition=compute          								 				  								  	     		    # Queue selection
 #SBATCH --job-name=yrv_trimgalore         							 															        # Job name
-#SBATCH --mail-type=ALL              							   				     									     		# Mail events (BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=yaamini.venkataraman@whoi.edu    				    									    		# Where to send mail
-#SBATCH --nodes=1                                                									        # One node
-#SBATCH --exclusive                                                 								      # All 36 procs on the one node
-#SBATCH --mem=100gb                                                 								      # Job memory request
-#SBATCH --time=24:00:00            								   															     		# Time limit hrs:min:sec
+#SBATCH --mail-type=ALL              							   				     									     		    # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=yaamini.venkataraman@whoi.edu    				    									    		    # Where to send mail
+#SBATCH --nodes=1                                                									            # One node
+#SBATCH --exclusive                                                 								          # All 36 procs on the one node
+#SBATCH --mem=100gb                                                 								          # Job memory request
+#SBATCH --time=24:00:00            								   															     	    	# Time limit hrs:min:sec
 #SBATCH --output=yrv_trimgalore%j.log  								   															     		# Standard output/error
-#SBATCH --chdir=/vortexfs1/scratch/yaamini.venkataraman/wc-green-crab/output/06b-trimgalore	# Working directory for this script
+#SBATCH --chdir=/vortexfs1/scratch/yaamini.venkataraman/wc-green-crab/output/06b-trimgalore	  # Working directory for this script
 
 #Adapted from script by Sam Bogan: https://github.com/snbogan/CP_RNAseq/blob/main/Scripts/CP_cutadapt.txt
 
@@ -38,24 +38,21 @@ for input_file in ${DATA_DIR}/*_R1_001.fastq.gz; do
   filename=$(basename "$input_file")
  # Construct the corresponding R2 filename
   input_r2="${input_file/_R1/_R2}"
-  # Construct the output filenames
-  output_r1="${OUTPUT_DIR}/${filename/R1_001.fastq.gz/_trimmed_R1.fastq}"
-  output_r2="${OUTPUT_DIR}/${filename/R1_001.fastq.gz/_trimmed_R2.fastq}"
   # Run TrimGalore to trim adapters, remove low-quality sequences, and retain sequences of a specific length
     ${TRIMGALORE} \
+    --cores 8 \
     --output_dir ${OUTPUT_DIR} \
+    --paired \
     -a "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA" \
     -A "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT" \
     --quality 20 \
     --length 20 \
-    -o $output_r1 \
-    -p $output_r2 \
-    $input_file \
-    $input_r2
     --fastqc_args \
     "--outdir ${OUTPUT_DIR} \
     --threads 28" \
-    --path_to_cutadapt ${CUTADAPT}
+    --path_to_cutadapt ${CUTADAPT} \
+    $input_file \
+    $input_r2
   # Print completion message
   echo "Trimming complete for $filename"
 done
@@ -76,6 +73,7 @@ echo "Trim 2: Remove poly A tails"
 
 #Remove adapter sequences. Specify universal adapters instead of using --illumina
 ${TRIMGALORE} \
+--cores 8 \
 --output_dir ${OUTPUT_DIR} \
 --paired \
 --fastqc_args \
