b24e46bbf8a0f586d7a8c72370bfc7a1c228eb94
Yaamini Venkataraman <yaaminiv@uw.edu>
2025-12-09 11:00:56 -0800

    slightly diff p values but same pathways enriched

--- output/05-metabolomics-analysis/metaboanalyst/5-earlyvlate_metaboanalyst-results/Rhistory.R
+++ output/05-metabolomics-analysis/metaboanalyst/5-earlyvlate_metaboanalyst-results/Rhistory.R
@@ -1,6 +1,6 @@
-# PID of current job: 19342
+# PID of current job: 2402160
 mSet<-InitDataObjects("conc", "pathora", FALSE, 150)
-cmpd.vec<-c("2,5-dihydroxypyrazine","2-hydroxyglutaric acid","3-phosphoglycerate","6-deoxygalactofuranose","UDP-N-acetylglucosamine","adenine","adenosine","adenosine-5-monophosphate","alpha-ketoglutarate","aminomalonic acid","ciliatine","citrulline","cysteine","cytidine-5-monophosphate","cytosine","dehydroascorbic acid","ethanolamine","fructose","fructose-1-phosphate","fructose-6-phosphate","galactose-6-phosphate","glucose-1-phosphate","glucose-6-phosphate","glutathione","glycerol","glycine","histidine","hypotaurine","ketoisoleucine","lactose","levoglucosan","lysine","methanolphosphate","nicotinamide","ornithine","oxamic acid","phosphate","pyrophosphate","saccharic acid","salicylic acid","taurine","terephthalic acid","trehalose-6-phosphate","uric acid")
+cmpd.vec<-c("2,5-dihydroxypyrazine","2-hydroxyglutaric acid","3-phosphoglycerate","6-deoxygalactofuranose","UDP-N-acetylglucosamine","adenine","adenosine","adenosine-5-monophosphate","alpha-ketoglutarate","aminomalonic acid","ciliatine","citrulline","cysteine","cytidine-5-monophosphate","cytosine","dehydroascorbic acid","ethanolamine","fructose","fructose-1-phosphate","fructose-6-phosphate","galactose-6-phosphate","glucose-1-phosphate","glucose-6-phosphate","glutathione","glycerol","glycine","histidine","hypotaurine","ketoisoleucine","lactose","levoglucosan","lysine","methanolphosphate","nicotinamide","ornithine","phosphate","pyrophosphate","saccharic acid","taurine","terephthalic acid","trehalose-6-phosphate","uric acid")
 mSet<-Setup.MapData(mSet, cmpd.vec);
 mSet<-CrossReferencing(mSet, "name");
 mSet<-CreateMappingResultTable(mSet)
@@ -21,12 +21,11 @@ mSet<-PerformDetailMatch(mSet, "ketoisoleucine");
 mSet<-GetCandidateList(mSet);
 mSet<-PerformDetailMatch(mSet, "methanolphosphate");
 mSet<-GetCandidateList(mSet);
-mSet<-PerformDetailMatch(mSet, "oxamic acid");
-mSet<-GetCandidateList(mSet);
 mSet<-SetKEGG.PathLib(mSet, "dme", "current")
 mSet<-SetMetabolomeFilter(mSet, F);
 mSet<-CalculateOraScore(mSet, "rbc", "hyperg")
 mSet<-PlotPathSummary(mSet, F, "path_view_0_", "png", 150, width=NA, NA, NA )
 mSet<-PlotKEGGPath(mSet, "Starch and sucrose metabolism",576, 480, "png", NULL)
-mSet<-RerenderMetPAGraph(mSet, "zoom1764799299864.png",576.0, 480.0, 100.0)
+mSet<-RerenderMetPAGraph(mSet, "zoom1765306386132.png",576.0, 480.0, 100.0)
+mSet<-PlotKEGGPath(mSet, "Taurine and hypotaurine metabolism",576, 480, "png", NULL)
 mSet<-SaveTransformedData(mSet)
