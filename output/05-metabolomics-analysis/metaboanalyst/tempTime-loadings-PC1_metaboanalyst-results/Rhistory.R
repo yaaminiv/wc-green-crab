1eb4b89e55bbd20f88ade3878636b4ae450e7894
Yaamini Venkataraman <yaaminiv@uw.edu>
2025-12-09 10:19:11 -0800

    repeat with slightly different metabolite list

--- output/05-metabolomics-analysis/metaboanalyst/tempTime-loadings-PC1_metaboanalyst-results/Rhistory.R
+++ output/05-metabolomics-analysis/metaboanalyst/tempTime-loadings-PC1_metaboanalyst-results/Rhistory.R
@@ -1,6 +1,6 @@
-# PID of current job: 783586
+# PID of current job: 2070228
 mSet<-InitDataObjects("conc", "pathora", FALSE, 150)
-cmpd.vec<-c("pyrophosphate","adenine","glucose-1-phosphate","UDP-N-acetylglucosamine","cytidine-5-monophosphate","fructose-6-phosphate","glucose-6-phosphate","2,5-dihydroxypyrazine","adenosine","levoglucosan","glycerol","cytosine","fructose","2-hydroxyglutaric acid","alpha-ketoglutarate","saccharic acid","citric acid","fructose-1-phosphate","galactose-6-phosphate","ethanolamine","phenylalanine","2,4-diaminobutyric acid","glutathione","nicotinamide","isomaltose","butyrolactam")
+cmpd.vec<-c("pyrophosphate","adenine","glucose-1-phosphate","UDP-N-acetylglucosamine","cytidine-5-monophosphate","fructose-6-phosphate","glucose-6-phosphate","2,5-dihydroxypyrazine","adenosine","glycerol","cytosine","levoglucosan","fructose","alpha-ketoglutarate","2-hydroxyglutaric acid","fructose-1-phosphate","saccharic acid","citric acid","galactose-6-phosphate","ethanolamine","phenylalanine","2,4-diaminobutyric acid","isomaltose","glutathione","nicotinamide","maleimide")
 mSet<-Setup.MapData(mSet, cmpd.vec);
 mSet<-CrossReferencing(mSet, "name");
 mSet<-CreateMappingResultTable(mSet)
@@ -17,5 +17,5 @@ mSet<-SetMetabolomeFilter(mSet, F);
 mSet<-CalculateOraScore(mSet, "rbc", "hyperg")
 mSet<-PlotPathSummary(mSet, F, "path_view_0_", "png", 150, width=NA, NA, NA )
 mSet<-PlotKEGGPath(mSet, "Starch and sucrose metabolism",576, 480, "png", NULL)
-mSet<-RerenderMetPAGraph(mSet, "zoom1764800823024.png",576.0, 480.0, 100.0)
+mSet<-RerenderMetPAGraph(mSet, "zoom1765304285235.png",576.0, 480.0, 100.0)
 mSet<-SaveTransformedData(mSet)
