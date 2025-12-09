af151beb7186c4e8f25ff4f6d6bf00ab79c58200
Yaamini Venkataraman <yaaminiv@uw.edu>
2025-12-09 11:14:41 -0800

    no change to enrichment

--- output/05-metabolomics-analysis/metaboanalyst/5v25-late_metaboanalyst-results/Rhistory.R
+++ output/05-metabolomics-analysis/metaboanalyst/5v25-late_metaboanalyst-results/Rhistory.R
@@ -1,6 +1,6 @@
-# PID of current job: 171593
+# PID of current job: 2507351
 mSet<-InitDataObjects("conc", "pathora", FALSE, 150)
-cmpd.vec<-c("1,5-anhydroglucitol","1-monopalmitin","2,3-dihydroxypyridine","2,5-dihydroxypyrazine","2-hydroxyglutaric acid","2-ketoisocaproic acid","2-picolinic acid","3-Amino-2-piperidone","4-hydroxymandelic acid","5-hydroxynorvaline","6-deoxygalactofuranose","N-methyl-UMP","UDP-N-acetylglucosamine","alanine","aminomalonic acid","butyrolactam","cadaverine","ciliatine","citrulline","conduritol-beta-epoxide","creatinine","cytidine-5-monophosphate","dehydroascorbic acid","diphenoxyethane","ethanolamine","gluconic acid lactone","glucose","glucose-1-phosphate","glutathione","glycerol-alpha-phosphate","glycine","homoserine","inosine","inositol-4-monophosphate","ketoisoleucine","lactic acid","lactose","levoglucosan","maltose","maltotriose","melezitose","methionine","methionine sulfone","methionine sulfoxide","n-epsilon-trimethyllysine","ononitol","ornithine","oxoproline","panose","pentitol","phenol","phosphogluconic acid","pinitol","proline","pyrophosphate","sorbitol","trehalose-6-phosphate","uridine","xanthine")
+cmpd.vec<-c("1,5-anhydroglucitol","1-monopalmitin","2,3-dihydroxypyridine","2,5-dihydroxypyrazine","2-hydroxyglutaric acid","2-ketoisocaproic acid","2-picolinic acid","3-Amino-2-piperidone","4-hydroxymandelic acid","5-hydroxynorvaline","6-deoxygalactofuranose","N-methyl-UMP","UDP-N-acetylglucosamine","alanine","aminomalonic acid","butyrolactam","cadaverine","ciliatine","citrulline","conduritol-beta-epoxide","creatinine","cytidine-5-monophosphate","dehydroascorbic acid","diphenoxyethane","erythronic acid lactone","ethanolamine","gluconic acid lactone","glucose","glucose-1-phosphate","glutathione","glycerol-alpha-phosphate","glycine","homoserine","inosine","inositol-4-monophosphate","ketoisoleucine","lactic acid","lactose","levoglucosan","maltose","maltotriose","melezitose","methionine","methionine sulfone","methionine sulfoxide","n-epsilon-trimethyllysine","ononitol","ornithine","oxoproline","panose","pentitol","phenol","phosphogluconic acid","pinitol","proline","pyrophosphate","sorbitol","trehalose-6-phosphate","uridine","xanthine")
 mSet<-Setup.MapData(mSet, cmpd.vec);
 mSet<-CrossReferencing(mSet, "name");
 mSet<-CreateMappingResultTable(mSet)
@@ -8,12 +8,12 @@ mSet<-PerformDetailMatch(mSet, "2,3-dihydroxypyridine");
 mSet<-GetCandidateList(mSet);
 mSet<-PerformDetailMatch(mSet, "2,5-dihydroxypyrazine");
 mSet<-GetCandidateList(mSet);
-mSet<-SetCandidate(mSet, "2,5-dihydroxypyrazine", "2,5-Dihydroxypyridine");
 mSet<-PerformDetailMatch(mSet, "4-hydroxymandelic acid");
 mSet<-GetCandidateList(mSet);
-mSet<-PerformDetailMatch(mSet, "N-methyl-UMP");
-mSet<-GetCandidateList(mSet);
+mSet<-SetCandidate(mSet, "4-hydroxymandelic acid", "p-Hydroxymandelic acid");
 mSet<-PerformDetailMatch(mSet, "6-deoxygalactofuranose");
+mSet<-GetCandidateList(mSet);
+mSet<-PerformDetailMatch(mSet, "N-methyl-UMP");
 mSet<-GetCandidateList(mSet);
 mSet<-PerformDetailMatch(mSet, "conduritol-beta-epoxide");
 mSet<-GetCandidateList(mSet);
@@ -21,6 +21,8 @@ mSet<-PerformDetailMatch(mSet, "cytidine-5-monophosphate");
 mSet<-GetCandidateList(mSet);
 mSet<-SetCandidate(mSet, "cytidine-5-monophosphate", "Cytidine monophosphate");
 mSet<-PerformDetailMatch(mSet, "diphenoxyethane");
+mSet<-GetCandidateList(mSet);
+mSet<-PerformDetailMatch(mSet, "erythronic acid lactone");
 mSet<-GetCandidateList(mSet);
 mSet<-PerformDetailMatch(mSet, "glycerol-alpha-phosphate");
 mSet<-GetCandidateList(mSet);
@@ -40,6 +42,6 @@ mSet<-SetMetabolomeFilter(mSet, F);
 mSet<-CalculateOraScore(mSet, "rbc", "hyperg")
 mSet<-PlotPathSummary(mSet, F, "path_view_0_", "png", 150, width=NA, NA, NA )
 mSet<-PlotKEGGPath(mSet, "Starch and sucrose metabolism",576, 480, "png", NULL)
-mSet<-RerenderMetPAGraph(mSet, "zoom1764797889722.png",576.0, 480.0, 100.0)
+mSet<-RerenderMetPAGraph(mSet, "zoom1765307121050.png",576.0, 480.0, 100.0)
 mSet<-PlotKEGGPath(mSet, "Glutathione metabolism",576, 480, "png", NULL)
 mSet<-SaveTransformedData(mSet)
